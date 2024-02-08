ae3a56e800fa386effd0c5cb88d13416c04c5ac4
gigakhrz <kharazishvilig13@gmail.com>
2024-02-08 17:15:51 +0400

    add homewrapper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <HomeWrapper>Home</HomeWrapper>;
 };
 
 export default Home;
+
+const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
