549680b254faff5dd9b0af1fc7048b387a56111c
gigakhrz <kharazishvilig13@gmail.com>
2024-02-08 17:08:46 +0400

    add globalstyle in app and in app add appWrapper div

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+
 function App() {
-  return <></>;
+  return (
+    <AppWrapper>
+      <GlobalStyles />
+    </AppWrapper>
+  );
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+`;
