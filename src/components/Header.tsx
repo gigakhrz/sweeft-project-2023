adf76eb65d20dc6d108f6c3101f21d9938b791fb
gigakhrz <kharazishvilig13@gmail.com>
2024-02-13 18:31:15 +0400

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return <div>Header</div>;
+}
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  padding: 7, 30;
+  background-color: black;
+`;
