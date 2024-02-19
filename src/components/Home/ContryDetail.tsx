d29097ccb9297c15d7ed7dfccf298f8071b9866a
gigakhrz <kharazishvilig13@gmail.com>
2024-02-19 21:35:15 +0400

    bug, can not read undefined property

--- src/components/Home/ContryDetail.tsx
+++ src/components/Home/ContryDetail.tsx
@@ -4,28 +4,42 @@ import { useCountryStore } from "../../state/store";
 const ContryDetail = () => {
   const contryInfo = useCountryStore((store) => store.contryInfo);
 
-  const contry =
-    contryInfo !== null && contryInfo[0] !== null ? contryInfo[0] : {};
+  console.log(contryInfo);
+
+  if (!contryInfo) {
+    return <div>Loading...</div>; // Return a loading indicator or handle null case
+  }
+  const currency = contryInfo[0].currencies;
 
   return (
     <Info>
       <li>
         <h2>Capital</h2>
+        <h3>{contryInfo !== null ? contryInfo[0].capital : "N/A"}</h3>
       </li>
       <li>
         <h2>Continent</h2>
+        <h3>{contryInfo !== null ? contryInfo[0].continents : "N/A"}</h3>
       </li>
       <li>
         <h2>Currency</h2>
+        <h3>
+          {" "}
+          {currency?.[0]?.name ? currency?.[0]?.name : "N/A"}{" "}
+          {currency?.[0]?.symbol ? currency?.[0]?.symbol : "N/A"}
+        </h3>
       </li>
       <li>
         <h2>Population</h2>
+        <h3>{contryInfo !== null ? contryInfo[0].population : "N/A"}</h3>
       </li>
       <li>
         <h2>Region</h2>
+        <h3>{contryInfo !== null ? contryInfo[0].region : "N/A"}</h3>
       </li>
       <li>
         <h2>Borders</h2>
+        <h3>{contryInfo !== null ? contryInfo[0].borders : "N/A"}</h3>
       </li>
     </Info>
   );
@@ -43,9 +57,18 @@ const Info = styled.ul`
     width: 250px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
+    padding: 0 10px;
 
     & > h2 {
       font-weight: 700;
+      font-size: 16px;
+      line-height: 20px;
+      color: #5c5e60;
+    }
+
+    & > h3 {
+      font-weight: 500;
       font-size: 16px;
       line-height: 20px;
       color: #5c5e60;
