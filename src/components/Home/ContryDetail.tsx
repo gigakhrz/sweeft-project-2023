aef7583b26476fdf84afad1a3963dc599beecadc
gigakhrz <kharazishvilig13@gmail.com>
2024-02-19 21:19:08 +0400

    add some styles

--- src/components/Home/ContryDetail.tsx
+++ src/components/Home/ContryDetail.tsx
@@ -1,13 +1,54 @@
+import styled from "styled-components";
 import { useCountryStore } from "../../state/store";
 
 const ContryDetail = () => {
   const contryInfo = useCountryStore((store) => store.contryInfo);
 
+  const contry =
+    contryInfo !== null && contryInfo[0] !== null ? contryInfo[0] : {};
+
   return (
-    <div>
-      {contryInfo && contryInfo.length > 0 ? contryInfo[0].name.common : ""}
-    </div>
+    <Info>
+      <li>
+        <h2>Capital</h2>
+      </li>
+      <li>
+        <h2>Continent</h2>
+      </li>
+      <li>
+        <h2>Currency</h2>
+      </li>
+      <li>
+        <h2>Population</h2>
+      </li>
+      <li>
+        <h2>Region</h2>
+      </li>
+      <li>
+        <h2>Borders</h2>
+      </li>
+    </Info>
   );
 };
 
 export default ContryDetail;
+
+const Info = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: #add8e6;
+
+  li {
+    width: 250px;
+    display: flex;
+    align-items: center;
+
+    & > h2 {
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 20px;
+      color: #5c5e60;
+    }
+  }
+`;
