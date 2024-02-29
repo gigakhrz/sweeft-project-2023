a3485af21236c9cc180a3dbacd62c963972d7d9a
gigakhrz <kharazishvilig13@gmail.com>
2024-02-29 18:03:01 +0400

    add some styles

--- src/pages/Exchange.tsx
+++ src/pages/Exchange.tsx
@@ -1,9 +1,21 @@
+import styled from "styled-components";
+
 const Exchange = () => {
   return (
-    <div>
+    <CurrencyWrapper>
       <h1>Currency Exchange</h1>
-    </div>
+    </CurrencyWrapper>
   );
 };
 
 export default Exchange;
+
+const CurrencyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #add8e6;
+  max-width: 850px;
+  width: 100%;
+  border-radius: 10px;
+`;
