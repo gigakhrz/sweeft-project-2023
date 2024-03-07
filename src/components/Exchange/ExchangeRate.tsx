cd2406d413db0a6aee0f9e5f2399c39dc6e1e321
gigakhrz <kharazishvilig13@gmail.com>
2024-03-07 22:11:47 +0400

    add default component

---
+++ src/components/Exchange/ExchangeRate.tsx
@@ -0,0 +1,15 @@
+import { TextField } from "@mui/material";
+import styled from "styled-components";
+
+const ExchangeRate = () => {
+  return <div>ExchangeRate</div>;
+};
+
+export default ExchangeRate;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
